6ef49ad2a08778b235324b4dcc24fdf3f20ea14c
kts of kettek <kts@kettek.net>
2022-05-31 21:20:06 -0700

    Ignore root; adjust styling

--- frontend/src/sections/ArchPalette/Node.svelte
+++ frontend/src/sections/ArchPalette/Node.svelte
@@ -21,32 +21,36 @@
 </script>
 
 <main>
-  <section style='padding-left: {depth/2}em' class:archetype={isArchetype} on:click={onClick} class:selected={$paletteStore.focused===(isArchetype?node.Archetype.Self:'')}>
-    {#if !isArchetype}
-      <article class='opener'>
-        {#if open}
-        -
-        {:else}
-        +
-        {/if}
-      </article>
-    {:else}
-      <article>
-        {#await animationsStore.getImage(node.Archetype.Anim, node.Archetype.Face)}
-          ...
-        {:then bytes}
-          <img src="data:image/png;base64,{bytes}">
-        {:catch error}
-          {error}
-        {/await}
-      </article>
-    {/if}
-    {#if path}
-      <span>{path}</span>
-    {:else}
-      <span>root</span>
-    {/if}
-  </section>
+  {#if fullpath !== ""}
+    <section style='padding-left: {depth/2}em' class:archetype={isArchetype} on:click={onClick} class:selected={$paletteStore.focused===(isArchetype?node.Archetype.Self:'')}>
+      {#if !isArchetype}
+        <article class='opener'>
+          {#if open}
+          -
+          {:else}
+          +
+          {/if}
+        </article>
+      {:else}
+        <article class='image'>
+          {#await animationsStore.getImage(node.Archetype.Anim, node.Archetype.Face)}
+            ...
+          {:then bytes}
+            {#if bytes}
+              <img src="data:image/png;base64,{bytes}">
+            {/if}
+          {:catch error}
+            {error}
+          {/await}
+        </article>
+      {/if}
+      {#if path}
+        <span>{path}</span>
+      {:else}
+        <span>root</span>
+      {/if}
+    </section>
+  {/if}
   {#if node.Archetype === undefined && open}
     <ul>
       {#each Object.keys(node) as path2}
@@ -67,11 +71,26 @@
   article {
     min-width: 1em;
   }
+  .image {
+    min-width: 1.5em;
+    min-height: 1.5em;
+    border: 1px solid #333;
+    display: grid;
+    align-items: center;
+    justify-content: center;
+    box-sizing: content-box;
+    margin: 1px;
+  }
+  .image img {
+    image-rendering: pixelated;
+    zoom: 2;
+  }
   section {
     display: grid;
     grid-template-columns: auto minmax(0, 1fr);
     grid-template-rows: minmax(0, 1fr);
     text-align: left;
+    align-items: flex-end;
   }
   section.selected {
     background: rgba(128, 128, 128, 0.5)

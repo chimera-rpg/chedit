0ffb66d793d56b68c6dfa9c5ef9bedd869ad14b0
kts of kettek <kts@kettek.net>
2022-05-31 21:46:53 -0700

    Use menus components

--- frontend/src/sections/MainMenu.svelte
+++ frontend/src/sections/MainMenu.svelte
@@ -1,8 +1,27 @@
 <script lang='ts'>
+  import Menus from "../components/Menus/Menus.svelte"
+  import MenuBar from "../components/Menus/MenuBar.svelte"
+  import MenuList from "../components/Menus/MenuList.svelte"
+  import MenuItem from "../components/Menus/MenuItem.svelte"
+
+  function openMap() {
+    console.log('TODO: Show open map dialog')
+  }
 </script>
 
 <nav>
-  Main MENu
+  <Menus>
+    <MenuBar>
+      <MenuItem popup='map-menu'>
+        Maps
+        <MenuList popup='map-menu'>
+          <MenuItem on:click={openMap}>
+            Open...
+          </MenuItem>
+        </MenuList>
+      </MenuItem>
+    </MenuBar>
+  </Menus>
 </nav>
 
 <style>

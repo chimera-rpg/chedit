2146fcc7ceeb8131f8d68dde01f97fffa49959e9
kts of kettek <kts@kettek.net>
2022-07-26 15:41:54 -0700

    Add some type-specific fields

--- frontend/src/sections/Maps/ArchPreEditor.svelte
+++ frontend/src/sections/Maps/ArchPreEditor.svelte
@@ -156,50 +156,118 @@
           </select>
         </label>
 
-        <label>
-          <span>Anim</span>
-          <input value={cloned.Anim??''} placeholder={arch.Compiled.Anim} on:change={e=>change('Anim', e.target.value)}/>
-        </label>
-        <label>
-          <span>Face</span>
-          <input value={cloned.Face??''} placeholder={arch.Compiled.Face} on:change={e=>change('Face', e.target.value)}/>
-        </label>
+        {#if (cloned.Type??arch.Compiled.Type) == 'Exit'}
+          <fieldset>
+            <legend>Exit</legend>
+            <label>
+              <span>Name</span>
+              <input value={cloned.Exit?.Name??''} placeholder={arch.Compiled.Exit?.Name} on:change={e=>change('Exit.Name', e.target.value)}/>
+            </label>
+            <label>
+              <span>Y</span>
+              <input type='number' value={cloned.Exit?.Y??''} placeholder={arch.Compiled.Exit?.Y} on:change={e=>change('Exit.Y', e.target.value)}/>
+            </label>
+            <label>
+              <span>X</span>
+              <input type='number' value={cloned.Exit?.X??''} placeholder={arch.Compiled.Exit?.X} on:change={e=>change('Exit.X', e.target.value)}/>
+            </label>
+            <label>
+              <span>Z</span>
+              <input type='number' value={cloned.Exit?.Z??''} placeholder={arch.Compiled.Exit?.Z} on:change={e=>change('Exit.Z', e.target.value)}/>
+            </label>
+            <label>
+              <span>Touch</span>
+              <input type='checkbox' checked={cloned.Exit?.Touch??''} placeholder={arch.Compiled.Exit?.Touch} on:change={e=>change('Exit.Touch', e.target.checked)}/>
+            </label>
+            <label>
+              <span>Cooldown</span>
+              <input value={cloned.Exit?.Cooldown??''} placeholder={arch.Compiled.Exit?.Cooldown} on:change={e=>change('Exit.Cooldown', e.target.value)}/>
+            </label>
+            <label>
+              <span>Size Ratio</span>
+              <input type='number' value={cloned.Exit?.SizeRatio??''} placeholder={arch.Compiled.Exit?.SizeRatio} on:change={e=>change('Exit.SizeRatio', e.target.value)}/>
+            </label>
+            <label>
+              <span>Uses</span>
+              <input type='number' value={cloned.Exit?.Uses??''} placeholder={arch.Compiled.Exit?.Uses} on:change={e=>change('Exit.Uses', e.target.value)}/>
+            </label>
+            <label>
+              <span>Unique Uses</span>
+              <input type='number' value={cloned.Exit?.UniqueUses??''} placeholder={arch.Compiled.Exit?.UniqueUses} on:change={e=>change('Exit.UniqueUses', e.target.value)}/>
+            </label>
+          </fieldset>
+        {/if}
 
-        <label>
-          <span>Height</span>
-          <input type='number' value={cloned.Height??''} placeholder={arch.Compiled.Height} on:change={e=>change('Height', e.target.value)}/>
-        </label>
-        <label>
-          <span>Width</span>
-          <input type='number' value={cloned.Width??''} placeholder={arch.Compiled.Width} on:change={e=>change('Width', e.target.value)}/>
-        </label>
-        <label>
-          <span>Depth</span>
-          <input type='number' value={cloned.Depth??''} placeholder={arch.Compiled.Depth} on:change={e=>change('Depth', e.target.value)}/>
-        </label>
+        {#if (cloned.Type??arch.Compiled.Type) == 'Audio'}
+          <fieldset>
+            <legend>Audio</legend>
+            <label>
+              <span>Audio</span>
+              <input value={cloned.Audio??''} placeholder={arch.Compiled.Audio} on:change={e=>change('Audio', e.target.value)}/>
+            </label>
+            <label>
+              <span>SoundSet</span>
+              <input value={cloned.SoundSet??''} placeholder={arch.Compiled.SoundSet} on:change={e=>change('SoundSet', e.target.value)}/>
+            </label>
+            <label>
+              <span>SoundIndex</span>
+              <input type='number' value={cloned.SoundIndex??''} placeholder={arch.Compiled.SoundIndex} on:change={e=>change('SoundIndex', e.target.value)}/>
+            </label>
+          </fieldset>
+        {/if}
 
-        <label>
-          <span>Matter</span>
-          <input value={cloned.Matter??''} placeholder={arch.Compiled.Matter} on:change={e=>change('Matter', e.target.value)}/>
-        </label>
-        <label>
-          <span>Blocking</span>
-          <input value={cloned.Blocking??''} placeholder={arch.Compiled.Blocking} on:change={e=>change('Blocking', e.target.value)}/>
-        </label>
+        <fieldset>
+          <legend>Appearance</legend>
+          <label>
+            <span>Anim</span>
+            <input value={cloned.Anim??''} placeholder={arch.Compiled.Anim} on:change={e=>change('Anim', e.target.value)}/>
+          </label>
+          <label>
+            <span>Face</span>
+            <input value={cloned.Face??''} placeholder={arch.Compiled.Face} on:change={e=>change('Face', e.target.value)}/>
+          </label>
+        </fieldset>
 
-        <label>
-          <span>Audio</span>
-          <input value={cloned.Audio??''} placeholder={arch.Compiled.Audio} on:change={e=>change('Audio', e.target.value)}/>
-        </label>
-        <label>
-          <span>SoundSet</span>
-          <input value={cloned.SoundSet??''} placeholder={arch.Compiled.SoundSet} on:change={e=>change('SoundSet', e.target.value)}/>
-        </label>
-        <label>
-          <span>SoundIndex</span>
-          <input type='number' value={cloned.SoundIndex??''} placeholder={arch.Compiled.SoundIndex} on:change={e=>change('SoundIndex', e.target.value)}/>
-        </label>
+        <fieldset>
+          <legend>Dimensionality</legend>
+          <label>
+            <span>Height</span>
+            <input type='number' value={cloned.Height??''} placeholder={arch.Compiled.Height} on:change={e=>change('Height', e.target.value)}/>
+          </label>
+          <label>
+            <span>Width</span>
+            <input type='number' value={cloned.Width??''} placeholder={arch.Compiled.Width} on:change={e=>change('Width', e.target.value)}/>
+          </label>
+          <label>
+            <span>Depth</span>
+            <input type='number' value={cloned.Depth??''} placeholder={arch.Compiled.Depth} on:change={e=>change('Depth', e.target.value)}/>
+          </label>
 
+          <label>
+            <span>Matter</span>
+            <input value={cloned.Matter??''} placeholder={arch.Compiled.Matter} on:change={e=>change('Matter', e.target.value)}/>
+          </label>
+          <label>
+            <span>Blocking</span>
+            <input value={cloned.Blocking??''} placeholder={arch.Compiled.Blocking} on:change={e=>change('Blocking', e.target.value)}/>
+          </label>
+        </fieldset>
+
+        <fieldset>
+          <legend>Events</legend>
+          {#if (cloned.Type??arch.Compiled.Type) == 'Exit'}
+            <details>
+              <summary>Exit</summary>
+              <label>
+                <span>Trigger</span>
+                <input value={cloned.Events?.Exit?.Trigger?.Event??''} placeholder={arch.Compiled.Events?.Exit?.Trigger?.Event} on:change={e=>change('Events.Exit.Trigger.Event', e.target.value)}/>
+              </label>
+              <!-- TODO: Spawn -->
+              <!-- TODO: Replace -->
+              <textarea value={cloned.Events?.Exit?.Script??''} placeholder={arch.Compiled.Events?.Exit?.Script}></textarea>
+            </details>
+          {/if}
+        </fieldset>
       </article>
     {/if}
     <div class='toolbar'>

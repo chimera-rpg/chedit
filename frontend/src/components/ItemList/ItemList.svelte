db3c5ae1faaca253a8664f1d34c82f278eca6326
kts of kettek <kts@kettek.net>
2022-08-12 17:42:00 -0700

    Add components for making item lists

---
+++ frontend/src/components/ItemList/ItemList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { setContext, createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  let startIndex = -1;
+  export let focusedIndex = -1;
+  setContext('SetIndex', {
+    index: (): number => {
+      startIndex += 1;
+      return startIndex
+    },
+    focusIndex: (index: number, data: any) => {
+      focusedIndex = index
+      dispatch('focus', index, data)
+    },
+    getFocusedIndex: (): number => {
+      return focusedIndex
+    },
+  })
+</script>
+
+<div>
+  <header>
+    <slot name='header'></slot>
+  </header>
+  <section>
+    <slot name='items'></slot>
+  </section>
+</div>
---
+++ frontend/src/components/ItemList/ItemListItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { getContext, createEventDispatcher } from "svelte"
+
+  export let id: any
+  let value = -1
+  const { index, focusIndex, getFocusedIndex } = getContext('SetIndex')
+  value = index()
+
+  const dispatch = createEventDispatcher()
+  function handleFocus() {
+    focusIndex(value, id)
+  }
+</script>
+
+<div on:click={handleFocus} class:focused={value===getFocusedIndex()}>
+  <slot></slot>
+</div>
+
+<style>
+  div.focused {
+    border: 1px solid red;
+  }
+</style>
